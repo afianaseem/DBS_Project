07731ad7ced247f9c90a6fbb53012a33f31e2902
afianaseem <130133063+afianaseem@users.noreply.github.com>
2024-06-10 11:37:32 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+module.exports = {
+    mongoURI: 'mongodb+srv://SummeyaTahir:921m03aa21@summeyatahir.teqkxhi.mongodb.net/?retryWrites=true&w=majority&appName=SummeyaTahir',
+    dbName: 'QuranicVersesInsight'
+};
